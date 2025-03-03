f1978aad7040f32305ca6d3aed2c099c5cdd5925
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-03 18:56:23 +0100

    testing the route functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+import userRoutes from './routes/user.route.js';
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
