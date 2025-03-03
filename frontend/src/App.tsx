2cbf2810b7482c8bbb958029a56ee131e8c4e297
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-03 18:24:08 +0100

    added clerk dependency to the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
       <h1 className="text-red-500 font-bold">Hello world</h1>
+      <Button>Button</Button>
     </>
   )
 }
