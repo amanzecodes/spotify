7df994e3564027a1efdfc32565d525b84e7943aa
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-03 18:35:52 +0100

    testing clerk implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <header>
       <SignedOut>
-        <SignInButton />
+       <SignInButton><Button>Sign in</Button></SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
