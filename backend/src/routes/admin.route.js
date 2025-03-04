dd5d74fd055caae3728fd96ac47073fa040beff0
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-04 17:22:22 +0100

    optimized code

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -3,11 +3,12 @@ import { createSong, deleteSong, createAlbum, deleteAlbum, checkAdmin } from "..
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
+router.use(protectRoute, requireAdmin)
 
-router.get('/check', protectRoute, requireAdmin, checkAdmin)
+router.get('/check',checkAdmin)
 
-router.post("/songs", protectRoute, requireAdmin, createSong);
-router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
-router.post("/albums", protectRoute, requireAdmin, createAlbum);
-router.delete("/albums/:id", protectRoute, requireAdmin, deleteAlbum);
+router.post("/songs",createSong);
+router.delete("/songs/:id",deleteSong);
+router.post("/albums",createAlbum);
+router.delete("/albums/:id",deleteAlbum);
 export default router;
