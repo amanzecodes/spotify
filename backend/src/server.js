7e61aa9b2bfc4db6933692aa8befa44dc394bd09
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-04 07:14:58 +0100

    added clerk middleware to the backend project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import { clerkMiddleware } from '@clerk/express';
 import { connectDB } from './lib/db.js';
 
 import userRoutes from './routes/user.route.js';
@@ -16,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(clerkMiddleware()); //this will add auth to request object
 app.use("/api/users", userRoutes)
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
