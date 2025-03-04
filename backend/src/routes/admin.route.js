2ba187fdd49c50326062117f46c78c2b40727614
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-04 15:53:11 +0100

    created the delete album route

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createSong, deleteSong, createAlbum, deleteAlbum } from "../controller/admin.controller.js";
+import { createSong, deleteSong, createAlbum, deleteAlbum, checkAdmin } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
+
+router.get('/check', protectRoute, requireAdmin, checkAdmin)
 
 router.post("/songs", protectRoute, requireAdmin, createSong);
 router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
