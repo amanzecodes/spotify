8fc34e627db9648696e58a477bf4d63857c3cecc
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-04 20:28:04 +0100

    done with the songs route controllers

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/song.controller.js";
+import { requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello from songs route');
-})
+router.get('/', protectRoute, requireAdmin, getAllSongs)
+router.get('/featured', getFeaturedSongs)
+router.get('/made-for-you', getMadeForYouSongs)
+router.get('/trending', getTrendingSongs)
 
 export default router;
