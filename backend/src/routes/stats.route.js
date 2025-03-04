ff43dc3a17bb227247c0ebd387b07053ce1e244a
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-04 21:01:12 +0100

    created the statistics route for the admin page

--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { stats } from "../controller/stats.controller.js";
 const router = Router();
- router.get('/stats', (req, res) => {
-    res.send('Stats route');
- })
+
+ router.get('/stats', protectRoute, requireAdmin, stats)
 
  export default router;
