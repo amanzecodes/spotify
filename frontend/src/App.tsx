21b8a9a02faea3711217281a29c56bf12792b299
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-05 16:14:52 +0100

    setting up react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,12 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import { Routes } from "react-router-dom"
+
 
 function App() {
   return (
     <>
-      <header>
-      <SignedOut>
-       <SignInButton><Button>Sign in</Button></SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+      <Routes>
+        
+      </Routes>
     </>
   )
 }
