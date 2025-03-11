08bec2c55cf4103b1454380ba610efc54950609d
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-11 12:02:55 +0100

    implementing oath redirect with clerk

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,29 @@
+import { SignedOut } from "@clerk/clerk-react"
+import { LayoutDashboardIcon } from "lucide-react"
+import { Link } from "react-router-dom"
+import SignInOAuthButton from "./SignInOAuthButton"
+
+const Topbar = () => {
+    const isAdmin = false
+  return (
+    <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75
+    backdrop-blur-md z-10">
+        <div className="flex gap-2 items-center">
+            Spotify
+        </div>
+        <div className="mr-2">
+            {isAdmin && (<Link to={"/admin"} className="flex items-center gap-4">
+            <LayoutDashboardIcon className="size-4"/>
+                Admin Dashboard
+            </Link>
+            )}
+            <SignedOut>
+                <SignInOAuthButton />
+            </SignedOut>
+        </div>
+    </div>
+
+  )
+
+}
+export default Topbar
