a849a71b9a171ed5faa5fab2d98d6b893ac6fd25
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-18 14:15:29 +0100

    created a global state for fetching songs and albums  with zustand

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -2,14 +2,21 @@ import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton";
 import { buttonVariants } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
+import { useMusicStore } from "@/stores/useMusicStore";
 import { SignedIn } from "@clerk/clerk-react";
 import { HomeIcon, Library, MessageCircle } from "lucide-react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const LeftSidebar = () => {
-  const isLoading = false;
+  
+  const { songs, albums, fetchAlbums, isLoading } = useMusicStore()
 
+  useEffect(() => {
+    fetchAlbums()
+  }, [fetchAlbums])
 
+  console.log({ albums})
   return (
     <div className="h-full flex flex-col gap-2">
       {/* Navigation Menu */}
