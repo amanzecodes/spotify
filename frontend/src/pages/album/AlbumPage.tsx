a200b29ba2ef303b0f5faae68ba41a4183a7484b
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-21 01:57:34 +0100

    Play Song Functionality & Queue added

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useMusicStore } from "@/stores/useMusicStore";
-import { Clock, Play } from "lucide-react";
+import { Clock, Pause, Play } from "lucide-react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { usePlayerStore } from "@/stores/usePlayerStore";
@@ -22,7 +22,17 @@ const AlbumPage = () => {
 
   if (isLoading) return <div>Loading...</div>;
 
-  const handlePlayAlbum = (index: number) => {
+  const handlePlayAlbum = () => {
+    if(!currentAlbum) return;
+    const isCurrentAlbumPlaying = currentAlbum?.songs.some(song => song._id === currentSong?._id);
+    if(isCurrentAlbumPlaying) togglePlay();
+    else {
+      //start playing from the beginning
+      playAlbum(currentAlbum?.songs, 0);
+    }
+  }
+
+  const handlePlaySong = (index: number) => {
     if(!currentAlbum) return
     playAlbum(currentAlbum?.songs, index);
   }
@@ -58,12 +68,15 @@ const AlbumPage = () => {
             </div>
 
             {/* play button */}
-            <div className="px-6 ob-4 flex items-center gap-6">
+            <div className="px-6 pb-4 flex items-center gap-6">
               <Button
+              onClick={handlePlayAlbum}
                 size="icon"
                 className="w-14 h-14 rounded-full bg-green-500 hover:bg-green-400 hover:scale-105 transition-all cursor-pointer"
               >
-                <Play className="h-7 w-7 text-black" />
+                {isPlaying && currentAlbum?.songs.some(song => song._id === currentSong?._id) ? (
+                  <Pause className="h-7 w-7 text-black"/>
+                ): <Play className="h-7 w-7 text-black" />}
               </Button>
             </div>
             {/* table Section */}
@@ -88,7 +101,7 @@ const AlbumPage = () => {
                     return(
                     <div
                       key={song._id}
-                      onClick={() => handlePlayAlbum(index)}
+                      onClick={() => handlePlaySong(index)}
                       className={`grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm 
                       text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer
                       `}
